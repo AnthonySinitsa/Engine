b6adfef19791d481d0330437431874f7ccaf3073
AnthonySinitsa <itonyemail@gmail.com>
2024-05-13 21:38:03 +0000

    updated applicationclass.h to have lightshaderclass and lightclass

--- Engine/applicationclass.h
+++ Engine/applicationclass.h
@@ -1,5 +1,3 @@
-#pragma once
-
 // The application class is the other object that is created by the system class. All the graphics functionality in this application will be encapsulated in this class. 
 // I will also use the header in this file for all the graphics related global settings that we may want to change such as full screen or windowed mode. 
 // Currently this class will be empty but in future tutorials will contain all the graphics objects.
@@ -15,6 +13,8 @@
 #include "cameraclass.h"
 #include "modelclass.h"
 #include "textureshaderclass.h"
+#include "lightshaderclass.h"
+#include "lightclass.h"
 
 
 /////////////
@@ -39,7 +39,7 @@ public:
 	bool Frame();
 
 private:
-	bool Render();
+	bool Render(float);
 
 private:
 
@@ -47,6 +47,8 @@ private:
 	CameraClass* m_Camera;
 	ModelClass* m_Model;
 	TextureShaderClass* m_TextureShader;
+	LightShaderClass* m_LightShader;
+	LightClass* m_Light;
 };
 
 #endif
