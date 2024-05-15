92895946158b69994218da9829a1d64ca3c7c6f6
AnthonySinitsa <itonyemail@gmail.com>
2024-05-15 06:06:40 +0000

    fix the nullptr bug

--- Engine/lightshaderclass.h
+++ Engine/lightshaderclass.h
@@ -1,10 +1,12 @@
 #ifndef _LIGHTSHADERCLASS_H_
 #define _LIGHTSHADERCLASS_H_
 
+
 /////////////
 // GLOBALS //
 /////////////
 const int NUM_LIGHTS = 4;
+
 
 //////////////
 // INCLUDES //
@@ -23,48 +25,48 @@ using namespace std;
 class LightShaderClass
 {
 private:
-    struct MatrixBufferType
-    {
-        XMMATRIX world;
-        XMMATRIX view;
-        XMMATRIX projection;
-    };
+	struct MatrixBufferType
+	{
+		XMMATRIX world;
+		XMMATRIX view;
+		XMMATRIX projection;
+	};
 
-    struct LightColorBufferType
-    {
-        XMFLOAT4 diffuseColor[NUM_LIGHTS];
-    };
+	struct LightColorBufferType
+	{
+		XMFLOAT4 diffuseColor[NUM_LIGHTS];
+	};
 
-    struct LightPositionBufferType
-    {
-        XMFLOAT4 lightPosition[NUM_LIGHTS];
-    };
+	struct LightPositionBufferType
+	{
+		XMFLOAT4 lightPosition[NUM_LIGHTS];
+	};
 
 public:
-    LightShaderClass();
-    LightShaderClass(const LightShaderClass&);
-    ~LightShaderClass();
+	LightShaderClass();
+	LightShaderClass(const LightShaderClass&);
+	~LightShaderClass();
 
-    bool Initialize(ID3D11Device*, HWND);
-    void Shutdown();
-    bool Render(ID3D11DeviceContext*, int, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*, XMFLOAT4[], XMFLOAT4[]);
-
-private:
-    bool InitializeShader(ID3D11Device*, HWND, WCHAR*, WCHAR*);
-    void ShutdownShader();
-    void OutputShaderErrorMessage(ID3D10Blob*, HWND, WCHAR*);
-
-    bool SetShaderParameters(ID3D11DeviceContext*, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*, XMFLOAT4[], XMFLOAT4[]);
-    void RenderShader(ID3D11DeviceContext*, int);
+	bool Initialize(ID3D11Device*, HWND);
+	void Shutdown();
+	bool Render(ID3D11DeviceContext*, int, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*, XMFLOAT4[], XMFLOAT4[]);
 
 private:
-    ID3D11VertexShader* m_vertexShader;
-    ID3D11PixelShader* m_pixelShader;
-    ID3D11InputLayout* m_layout;
-    ID3D11SamplerState* m_sampleState;
-    ID3D11Buffer* m_matrixBuffer;
-    ID3D11Buffer* m_lightColorBuffer;
-    ID3D11Buffer* m_lightPositionBuffer;
+	bool InitializeShader(ID3D11Device*, HWND, WCHAR*, WCHAR*);
+	void ShutdownShader();
+	void OutputShaderErrorMessage(ID3D10Blob*, HWND, WCHAR*);
+
+	bool SetShaderParameters(ID3D11DeviceContext*, XMMATRIX, XMMATRIX, XMMATRIX, ID3D11ShaderResourceView*, XMFLOAT4[], XMFLOAT4[]);
+	void RenderShader(ID3D11DeviceContext*, int);
+
+private:
+	ID3D11VertexShader* m_vertexShader;
+	ID3D11PixelShader* m_pixelShader;
+	ID3D11InputLayout* m_layout;
+	ID3D11SamplerState* m_sampleState;
+	ID3D11Buffer* m_matrixBuffer;
+	ID3D11Buffer* m_lightColorBuffer;
+	ID3D11Buffer* m_lightPositionBuffer;
 };
 
 #endif
