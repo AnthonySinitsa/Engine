f1181c751a7df2ec42e2e3c9f6a9a79afbf75bbd
AnthonySinitsa <itonyemail@gmail.com>
2024-05-16 02:47:28 +0000

    update inputclass

--- Engine/inputclass.h
+++ Engine/inputclass.h
@@ -1,5 +1,3 @@
-#pragma once
-
 // The input class handles the user input from the keyboard. This class is given input from the SystemClass::MessageHandler function. 
 // The input object will store the state of each key in a keyboard array.
 //  When queried it will tell the calling functions if a certain key is pressed.
@@ -7,6 +5,21 @@
 #ifndef _INPUTCLASS_H_
 #define _INPUTCLASS_H_
 
+///////////////////////////////
+// PRE-PROCESSING DIRECTIVES //
+///////////////////////////////
+#define DIRECTINPUT_VERSION 0x0800
+
+/////////////
+// LINKING //
+/////////////
+#pragma comment(lib, "dinput8.lib")
+#pragma comment(lib, "dxguid.lib")
+
+//////////////
+// INCLUDES //
+//////////////
+#include <dinput.h>
 
 ////////////////////////////////////////////////////////////////////////////////
 // Class name: InputClass
@@ -14,19 +27,32 @@
 class InputClass
 {
 public:
-	InputClass();
-	InputClass(const InputClass&);
-	~InputClass();
+    InputClass();
+    InputClass(const InputClass&);
+    ~InputClass();
 
-	void Initialize();
+    bool Initialize(HINSTANCE, HWND, int, int);
+    void Shutdown();
+    bool Frame();
 
-	void KeyDown(unsigned int);
-	void KeyUp(unsigned int);
-
-	bool IsKeyDown(unsigned int);
+    bool IsEscapePressed();
+    void GetMouseLocation(int&, int&);
+    bool IsMousePressed();
 
 private:
-	bool m_keys[256];
+    bool ReadKeyboard();
+    bool ReadMouse();
+    void ProcessInput();
+
+private:
+    IDirectInput8* m_directInput;
+    IDirectInputDevice8* m_keyboard;
+    IDirectInputDevice8* m_mouse;
+
+    unsigned char m_keyboardState[256];
+    DIMOUSESTATE m_mouseState;
+
+    int m_screenWidth, m_screenHeight, m_mouseX, m_mouseY;
 };
 
 #endif
