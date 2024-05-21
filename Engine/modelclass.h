97a89cdbcbaa9f9dd8fbe5f7ef1f2c88a9934306
AnthonySinitsa <itonyemail@gmail.com>
2024-05-21 21:53:03 +0000

    updated model class

--- Engine/modelclass.h
+++ Engine/modelclass.h
@@ -12,60 +12,49 @@ using namespace DirectX;
 using namespace std;
 
 
-///////////////////////
-// MY CLASS INCLUDES //
-///////////////////////
-#include "textureclass.h"
-
-
 ////////////////////////////////////////////////////////////////////////////////
 // Class name: ModelClass
 ////////////////////////////////////////////////////////////////////////////////
 class ModelClass
 {
 private:
-	struct VertexType
-	{
-		XMFLOAT3 position;
-		XMFLOAT2 texture;
-		XMFLOAT3 normal;
-	};
+    struct VertexType
+    {
+        XMFLOAT3 position;
+        XMFLOAT2 texture;
+        XMFLOAT3 normal;
+    };
 
-	struct ModelType
-	{
-		float x, y, z;
-		float tu, tv;
-		float nx, ny, nz;
-	};
+    struct ModelType
+    {
+        float x, y, z;
+        float tu, tv;
+        float nx, ny, nz;
+    };
 
 public:
-	ModelClass();
-	ModelClass(const ModelClass&);
-	~ModelClass();
+    ModelClass();
+    ModelClass(const ModelClass&);
+    ~ModelClass();
 
-	bool Initialize(ID3D11Device*, ID3D11DeviceContext*, char*, char*);
-	void Shutdown();
-	void Render(ID3D11DeviceContext*);
+    bool Initialize(ID3D11Device*, ID3D11DeviceContext*, char*);
+    void Shutdown();
+    void Render(ID3D11DeviceContext*);
 
-	int GetIndexCount();
-	ID3D11ShaderResourceView* GetTexture();
-
-private:
-	bool InitializeBuffers(ID3D11Device*);
-	void ShutdownBuffers();
-	void RenderBuffers(ID3D11DeviceContext*);
-
-	bool LoadTexture(ID3D11Device*, ID3D11DeviceContext*, char*);
-	void ReleaseTexture();
-
-	bool LoadModel(char*);
-	void ReleaseModel();
+    int GetIndexCount();
 
 private:
-	ID3D11Buffer* m_vertexBuffer, * m_indexBuffer;
-	int m_vertexCount, m_indexCount;
-	TextureClass* m_Texture;
-	ModelType* m_model;
+    bool InitializeBuffers(ID3D11Device*);
+    void ShutdownBuffers();
+    void RenderBuffers(ID3D11DeviceContext*);
+
+    bool LoadModel(char*);
+    void ReleaseModel();
+
+private:
+    ID3D11Buffer* m_vertexBuffer, * m_indexBuffer;
+    int m_vertexCount, m_indexCount;
+    ModelType* m_model;
 };
 
 #endif
